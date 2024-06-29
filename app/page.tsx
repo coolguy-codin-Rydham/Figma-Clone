145155d17774e1cbd8c5d05a979de1decce04a26
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-06-29 13:53:04 +0530

    Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { CollaborativeApp } from "./CollaborativeApp";
+import { Room } from "./Room";
+
+const page = () => {
+  return (
+  <Room>
+    <CollaborativeApp/>
+  </Room>
+  );
+};
+
+export default page;
